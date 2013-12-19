f4404472e766d32387c41aa8e566066aed04f98b
Vilmos Ioo <vilmosi@blinkbox.com>
2013-12-19 16:42:48 +0000

    Added features taxonomy.

--- inc/Demo_Custom_Post.php
+++ inc/Demo_Custom_Post.php
@@ -60,8 +60,47 @@ class Demo_Custom_Post{
 		
 		register_post_type( $this->slug, $args );
 		
-		add_action( 'right_now_content_table_end' , array(&$this, 'add_to_dashboard') );
+		$this->add_custom_taxonomy();
 
+		add_action( 'right_now_content_table_end' , array(&$this, 'add_to_dashboard') );
+	}
+
+	public function add_custom_taxonomy(){
+		$name = 'Feature';	
+		$slug = 'features';
+		$name_plural = 'Features';
+
+		// Add new taxonomy, NOT hierarchical (like tags)
+		$labels = array(
+			'name'                       => _x( $name_plural, 'taxonomy general name' ),
+			'singular_name'              => _x( $name, 'taxonomy singular name' ),
+			'search_items'               => __( 'Search '.$name_plural ),
+			'popular_items'              => __( 'Popular '.$name_plural ),
+			'all_items'                  => __( 'All '.$name_plural ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit '.$name ),
+			'update_item'                => __( 'Update '.$name ),
+			'add_new_item'               => __( 'Add New '.$name ),
+			'new_item_name'              => __( 'New '.$name.' Name' ),
+			'separate_items_with_commas' => __( 'Separate '.strtolower($name_plural).' with commas' ),
+			'add_or_remove_items'        => __( 'Add or remove '.strtolower($name_plural) ),
+			'choose_from_most_used'      => __( 'Choose from the most used '.strtolower($name_plural) ),
+			'not_found'                  => __( 'No '.strtolower($name_plural).' found.' ),
+			'menu_name'                  => __( $name_plural ),
+		);
+
+		$args = array(
+			'hierarchical'          => false,
+			'labels'                => $labels,
+			'show_ui'               => true,
+			'show_admin_column'     => true,
+			'update_count_callback' => '_update_post_term_count',
+			'query_var'             => true,
+			'rewrite'               => array( 'slug' => $slug ),
+		);
+
+		register_taxonomy( $slug, $this->slug, $args );
 	}
 
 	public function add_to_dashboard(){
